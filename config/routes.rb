77fc42b8b8706cd3705eaac1123d5f03ef7aef81
yuusukeakasaka <ayazato2206a@gmail.com>
2021-07-20 12:27:40 +0000

    必要ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  root "homes#top"
 end
