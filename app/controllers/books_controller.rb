abe78010c8f0d773b71371a2fc32351153f61075
yuusukeakasaka <ayazato2206a@gmail.com>
2021-07-24 09:19:58 +0000

    完成提出用

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,54 @@
 class BooksController < ApplicationController
   def index
+     @books = Book.all
+     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
-  def edit
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:success] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:success] = "Book was successfully destroyed."
+    redirect_to books_path(@book)
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:success] = "Book was successfully updated."
+       redirect_to book_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
-  root "homes#top"
+
+ resources :books
+ root "homes#top"
 end
